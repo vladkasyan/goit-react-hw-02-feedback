4a2782f7a30401c008c04c9d32e20cc17f25fc4a
vladkasyan <voadkasyan.2006@gmail.com>
2023-09-02 13:30:29 +0300

    hw

---
+++ src/components/feedbackOptions/feedbackOptions.module.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const list = styled.ul`
+display: flex;
+gap: 10px;
+`;
+
+export const button = styled.button`
+  cursor: pointer;
+
+  border-radius: 5px;
+  padding: 0.25em 1em;
+`;
